5470ddb7477cf9c36b002e31374d527a44b7adb6
Prinfe <emmyoode2001@gmail.com>
2022-09-15 11:32:12 -0700

    task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - fumction that multiplies two integers
+ * @a: first parameter
+ * @b: second parameter
+ * Return: value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
