e20c1bef02798782772b681ba0d7f8c42371bb64
Prinfe <emmyoode2001@gmail.com>
2022-09-23 18:02:37 -0700

    task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H 
- #define MAIN_H 
- int _putchar (char c); 
- #include <stdio.h> 
+#define MAIN_H 
+int _putchar (char c); 
+#include <stdio.h> 
  /** 
   * void prototypes() 
   * int prototypes() 
