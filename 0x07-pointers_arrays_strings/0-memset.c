a0222e4fd0892d9bd1d31686f869a3bce43a476f
Prinfe <emmyoode2001@gmail.com>
2022-09-26 11:25:28 -0700

    task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,19 +6,19 @@
 * @n: number of bytes to fill
 * Return: memory area
 */
-  
-char *_memset(char *s, char b, unsigned int n) 
-{ 
-         int size = n;
-  
-         if (size > 0)
-         {
-                 int i;
-  
-                 for (i = 0; i < size; i++)
-                 {
-                         s[i] = b;
-                 }
-         }
-         return (s);
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	 {
+		int i;
+
+		for (i = 0; i < size; i++)
+                {
+	 		s[i] = b;
+		}
+	 }
+	return (s);
 }
