6a008336b10a513609db2231e409c14fbf211025
Emmanuel Ode <emmyoode2001@gmail.com>
2022-09-28 06:50:59 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,7 +7,7 @@
   *      */ 
  void _print_rev_recursion(char *s) 
 	 { 
-		          if (*s) /*If statement*/ 
+		          if (*s != '\0') /*If statement*/ 
 			           { 
 				                    _print_rev_recursion(s + 1); /*add s*/ 
 					                     _putchar(*s); /*print s*/ 
