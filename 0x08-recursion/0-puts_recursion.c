310155692c493d4bfbd471587c2c57fe13152bdb
Emmanuel Ode <emmyoode2001@gmail.com>
2022-09-28 02:02:34 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,9 +4,9 @@
 *@s: pointer block of memory to fill
 *Return: void
 */
-void _puts_recursion(char *s)
+void _puts_recursion(char * s)
 {
-        if (*s == '\0')
+        if ( * s == '\0')
         {
                 _putchar('\n');
                 return;
