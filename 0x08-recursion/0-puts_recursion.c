93513782aa10be2d730537a8f7044eaee51f303e
Emmanuel Ode <emmyoode2001@gmail.com>
2022-09-28 14:29:10 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  _put_recursion - prints a string followed by a new line
+ * _put_recursion - a function that prints a string followed by a new line
  * @s: string to be printed
  *
  * Return: void
